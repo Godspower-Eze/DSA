40889e3d283d1b2f074d513d6ba886d1c193fba2
Godspower Eze <61994334+Godspower-Eze@users.noreply.github.com>
2026-08-18 23:28:26 +0300

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.rs
@@ -0,0 +1,14 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
+        let mut nums_map: HashMap<i32, i32> = HashMap::new();
+        for j in nums {
+            *nums_map.entry(j).or_insert(0) += 1;
+        }
+        let mut sorted: Vec<_> = nums_map.iter().collect();
+        sorted.sort_by(|a, b| b.1.cmp(a.1));
+        let result: Vec<i32> = sorted.into_iter().map(|a| *a.0 ).collect();
+        result[..(k as usize)].to_vec()
+    }
+}
