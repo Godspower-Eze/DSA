dece5fd43d38b16e1b6bc1ecfdb9be6e34b7c626
Godspower Eze <61994334+Godspower-Eze@users.noreply.github.com>
2026-08-20 22:42:58 +0300

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn encode(strs: Vec<String>) -> String {
+        if strs.len() == 0 {
+            return String::from("empty");
+        }
+        strs.join("-1")
+    }
+
+    pub fn decode(s: String) -> Vec<String> {
+        if s == String::from("empty") {
+            return vec![];
+        }
+        s.split("-1").map(|x| x.to_string()).collect()
+    }
+}
